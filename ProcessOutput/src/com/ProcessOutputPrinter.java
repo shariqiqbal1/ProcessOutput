4a784a52f6ccd51c3ecf0b4d83948bd7a4e720f5
Shariq <shariq.iqbal@dell.com>
2017-08-28 12:07:39 -0500

    merging stderr with stdout

--- ProcessOutput/src/com/ProcessOutputPrinter.java
+++ ProcessOutput/src/com/ProcessOutputPrinter.java
@@ -6,25 +6,20 @@ import java.io.InputStreamReader;
 public class ProcessOutputPrinter {
 	
 	public void printProcessOutput(Process process) {
-		Thread errThread = new Thread(() -> {
-		    try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
-		    	while ((reader.readLine()) != null)
+	    
+		Thread outThread = new Thread(() -> {
+		    try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+	            while ((reader.readLine()) != null)
 	                System.out.println(reader.readLine());
-		    } catch (Exception e) {
-		    }
+	        } catch (Exception e) {
+	        }
 		});
-		errThread.start();
-
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-			while ((reader.readLine()) != null)
-                System.out.println(reader.readLine());
-		} catch (Exception e) {
-		}
+		outThread.start();
 
 		int exitCode = -1;
 		try {
 		    exitCode = process.waitFor();
-		    errThread.join();
+		    outThread.join();
 		} catch (Exception e) {
 		}
 		
--- ProcessOutput/src/com/ProcessRunner.java
+++ ProcessOutput/src/com/ProcessRunner.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 public class ProcessRunner {
 
 	public static void main(String[] args) throws IOException {
+				
+		ProcessBuilder builder = new ProcessBuilder("sh", "C:\\Users\\shariq_iqbal\\workspace\\ReadProcessOutput\\ProcessOutput\\src\\com\\print.sh");		
 		
-		ProcessBuilder builder = new ProcessBuilder("sh", "/Users/shariqiqbal/Documents/workspace/ProcessOutput/ProcessOutput/src/com/print.sh");
+		builder.redirectErrorStream(true); //make sure to call redirectErrorStream(true) on the process builder object so that the processes' error output gets megred with standard output 
+		
 		Process process = builder.start();
 		
 		ProcessOutputPrinter processOutputPrinter = new ProcessOutputPrinter();
